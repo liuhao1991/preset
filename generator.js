7c7f108413e7b7345697be4de5206994e40579ed
liuhao <823232600@qq.comm>
2021-07-18 09:55:00 +0800

    update

--- generator.js
+++ generator.js
@@ -7,12 +7,12 @@ module.exports = (api, options, rootOptions) => {
     },
     'devDependencies': {
       '@babel/plugin-proposal-optional-chaining': '^7.9.0',
-      '@commitlint/cli': '^8.3.5',
-      '@commitlint/config-conventional': '^8.3.4',
-      'commitizen': '^4.0.4',
+      // '@commitlint/cli': '^8.3.5',
+      // '@commitlint/config-conventional': '^8.3.4',
+      // 'commitizen': '^4.0.4',
       'compression-webpack-plugin': '^3.1.0',
       'cross-env': '^7.0.2',
-      'cz-conventional-changelog': '^3.1.0',
+      // 'cz-conventional-changelog': '^3.1.0',
       'vue-cli-plugin-webpack-bundle-analyzer': '~2.0.0',
     },
     'scripts': {
@@ -22,25 +22,25 @@ module.exports = (api, options, rootOptions) => {
       'test:e2e': 'cross-env NODE_ENV=test vue-cli-service test:e2e',
       'lint': 'vue-cli-service lint src/**/*.{js,vue} tests/**/*.js --fix'
     },
-    'config': {
-      'commitizen': {
-        'path': 'node_modules/cz-conventional-changelog'
-      }
-    },
-    'gitHooks': {
-      'pre-commit': 'lint-staged',
-      'commit-msg': 'commitlint -e $GIT_PARAMS'
-    },
-    'lint-staged': {
-      'src/**/*.{js,jsx,vue}': [
-        'vue-cli-service lint --fix',
-        'git add'
-      ],
-      'tests/**/*.js': [
-        'vue-cli-service lint --fix',
-        'git add'
-      ]
-    }
+    // 'config': {
+    //   'commitizen': {
+    //     'path': 'node_modules/cz-conventional-changelog'
+    //   }
+    // },
+    // 'gitHooks': {
+    //   'pre-commit': 'lint-staged',
+    //   'commit-msg': 'commitlint -e $GIT_PARAMS'
+    // },
+    // 'lint-staged': {
+    //   'src/**/*.{js,jsx,vue}': [
+    //     'vue-cli-service lint --fix',
+    //     'git add'
+    //   ],
+    //   'tests/**/*.js': [
+    //     'vue-cli-service lint --fix',
+    //     'git add'
+    //   ]
+    // }
   })
 
   api.render('./template')
