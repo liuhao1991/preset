36100e6c939c3ed7585f142ebe2bfa6801c378d7
liuhao <823232600@qq.comm>
2021-07-18 15:21:31 +0800

    update

--- generatpr/index.js
+++ generatpr/index.js
@@ -1,63 +1,90 @@
-const fs = require('fs');
-// XXX: require('lignator') will only work with "vue add my-vue-preset" or "npm install --save-dev file:/local/path/to/my-vue-preset"
-// + "vue invoke my-vue-preset" but not with "vue create my-vue-project --preset my-vue-preset",
-// i.e. it will work if this project is used as a vue-cli plugin but not as a vue-cli preset (no project dependencies will be loaded!),
-// so for now we provide a local lignator file as a fallback plan.
-// To be used as vue-cli plugin, note that this project should be named in package.json as @MUTASANTE/vue-cli-plugin-preset-tpl
-// and registered on npmjs.com as @MUTASANTE/preset-tpl
-try {
-  var lignator = require('lignator');
-} catch (e) {
-  if (e instanceof Error && e.code === 'MODULE_NOT_FOUND')
-    lignator = require('./lignator');
-  else throw e;
-}
 
 module.exports = (api, options, rootOptions) => {
   // const v2 = options.vueVersion === 'v2';
-    // const v2 = options.version === 'v2';
+    const v2 = options.version === 'v2';
+    const mobile = options.platform === 'mobile';
     // options.ui = options.ui || 'none';
 
     // 添加依赖
     const dependencies = {
-        qs: '^6.7.0',
-        axios: '^0.21.0',
-        'tp-common.css': '^1.0.2',
+      qs: '^6.7.0',
+      axios: '^0.21.0',
+      'normalize.css': '^8.0.1',
+      'less-loader': '^7.3.0',
     };
-    // // UI 框架
-    // if (options.ui === 'element') {
-    //     if (v2) {
-    //         dependencies['element-ui'] = '^2.15.0';
-    //     } else {
-    //         dependencies['element-plus'] = '^1.0.1-beta.27';
-    //     }
-    // }
-    api.extendPackage({
-        dependencies,
-        devDependencies: {
-            'eslint-config-tpconfig': '^0.x',
-            'eslint-plugin-vue': '^7.0.0-0',
-            'style-resources-loader': '^1.3.2',
-            stylelint: '^13.9.0',
-            '@trscd/stylelint-config-tpconfig': '^0.x',
-            'stylelint-webpack-plugin': '^2.1.1',
-            'webpack-bundle-analyzer': '^4.3.0',
-            'babel-plugin-transform-remove-console': '^6.9.4',
-            'babel-plugin-transform-remove-debugger': '^6.9.4',
-        },
-    });
 
+    const devDependencies = {
+      "@vue/cli-plugin-babel": "~4.5.0",
+      "@vue/cli-plugin-eslint": "~4.5.0",
+      "@vue/cli-service": "~4.5.0",
+      "@vue/eslint-config-prettier": "^6.0.0",
+      "babel-eslint": "^10.1.0",
+      "babel-plugin-transform-remove-console": "^6.9.4",
+      "babel-plugin-transform-remove-debugger": "^6.9.4",
+      "eslint": "^6.7.2",
+      "eslint-config-tpconfig": "^0.x",
+      "eslint-plugin-prettier": "^3.3.1",
+      "eslint-plugin-vue": "^7.0.0-0",
+      "less": "^3.0.4",
+      "less-loader": "^5.0.0",
+      "prettier": "^2.2.1",
+      "style-resources-loader": "^1.3.2",
+      "stylelint": "^13.9.0",
+      "stylelint-webpack-plugin": "^2.1.1",
+      "vue-template-compiler": "^2.6.11",
+      "webpack-bundle-analyzer": "^4.3.0"
+    }
+    // // UI 框架
+    if (!mobile) {
+      if (v2) {
+        dependencies['element-ui'] = '^2.15.0';
+      } else {
+        dependencies['element-plus'] = '^1.0.1-beta.27';
+      }
+    } else {
+      if (v2) {
+        dependencies['vant'] = '^2.15.0';
+      } else {
+        dependencies['vant'] = '^3.0.0-beta.1';
+      }
+    }
     api.extendPackage({
+      dependencies,
+      devDependencies
+    });
+    if (v2) {
+      api.extendPackage({
         dependencies: {
-            vue: '^3.0.0',
-            'vue-router': '^4.0.0-0',
-            vuex: '^4.0.0-0',
+          'vue-router': '^3.4.9',
+          vuex: '^3.6.0',
+        },
+      });
+
+    } else {
+      api.extendPackage({
+        dependencies: {
+          vue: '^3.0.0',
+          'vue-router': '^4.0.0-0',
+          vuex: '^4.0.0-0',
         },
         devDependencies: {
-            'vue-template-compiler': null,
-            '@vue/compiler-sfc': '^3.0.0',
+          'vue-template-compiler': null,
+          '@vue/compiler-sfc': '^3.0.0',
         },
-    });
+      });
+    }
+
+    // api.extendPackage({
+    //   dependencies: {
+    //     vue: '^3.0.0',
+    //     'vue-router': '^4.0.0-0',
+    //     vuex: '^4.0.0-0',
+    //   },
+    //   devDependencies: {
+    //     'vue-template-compiler': null,
+    //     '@vue/compiler-sfc': '^3.0.0',
+    //   },
+    // });
 
     // 数据可视化大屏依赖
     // if (options.type === 'data-v') {
@@ -113,12 +140,6 @@ module.exports = (api, options, rootOptions) => {
 
     // 创建模板
     api.render('./template');
-
-    // 删除不必要的文件
-    const deletePath = [
-        'src/assets/logo.png',
-        'src/components/HelloWorld.vue',
-    ];
 
     api.render(files => {
         Object.keys(files).forEach(path => {
--- prompt.js
+++ prompt.js
@@ -1,1 +1,35 @@
-module.exports = [];
+
+module.exports = [
+  {
+    name: 'version',
+    type: 'list',
+    message: 'Choose a version of Vue.js',
+    choices: [
+        {
+          name: '2.x',
+          value: 'v2',
+        },
+        {
+          name: '3.x',
+          value: 'v3',
+        },
+    ],
+    default: 'v3',
+  },
+  {
+    name: 'platform',
+    type: 'list',
+    message: 'Choose whether your app is a PC or a mobile(default:pc)',
+    choices: [
+      {
+        name: 'PC',
+        value: 'pc'
+      },
+      {
+        name: 'mobile',
+        value: 'mobile'
+      },
+    ],
+    default: 'pc'
+  },
+];
