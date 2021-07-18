d8f9382303331056671c257983fcc1492d3fe39f
liuhao <823232600@qq.comm>
2021-07-18 23:42:36 +0800

    update

--- generator/template-base/.eslintrc.js
+++ generator/template-base/.eslintrc.js
@@ -1,22 +1,25 @@
-let extends = [];
-// <%_ if (options.version === 'v2') { _%>
-// extends = ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"];
-// <%_ } else { _%>
-extends = ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"];
-// <%_ } _%>
-
 module.exports = {
   root: true,
   env: {
     node: true,
   },
-  extends: extends,
+  extends: [
+    'eslint:recommended',
+    <%_ if (version === 'v2') { _%>
+    'plugin:vue/recommended',
+    <%_ } else { _%>
+    'plugin:vue/vue3-recommended',
+    <%_ } _%>
+    '@vue/standard',
+  ],
   parserOptions: {
     parser: "babel-eslint",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    <%_ if (version === 'v3') { _%>
     "vue/no-multiple-template-root": "off",
+    <%_ } _%>
   },
 };
