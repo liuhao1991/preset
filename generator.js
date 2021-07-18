59cc4134464247a4c226ed4af44566cc9318637c
liuhao <823232600@qq.comm>
2021-07-18 11:44:32 +0800

    update

--- generator.js
+++ generator.js
@@ -1,3 +1,18 @@
+const fs = require('fs');
+// XXX: require('lignator') will only work with "vue add my-vue-preset" or "npm install --save-dev file:/local/path/to/my-vue-preset"
+// + "vue invoke my-vue-preset" but not with "vue create my-vue-project --preset my-vue-preset",
+// i.e. it will work if this project is used as a vue-cli plugin but not as a vue-cli preset (no project dependencies will be loaded!),
+// so for now we provide a local lignator file as a fallback plan.
+// To be used as vue-cli plugin, note that this project should be named in package.json as @MUTASANTE/vue-cli-plugin-preset-tpl
+// and registered on npmjs.com as @MUTASANTE/preset-tpl
+try {
+  var lignator = require('lignator');
+} catch (e) {
+  if (e instanceof Error && e.code === 'MODULE_NOT_FOUND')
+    lignator = require('./lignator');
+  else throw e;
+}
+
 module.exports = (api, options, rootOptions) => {
   api.render(files => {
     Object.keys(files)
@@ -62,33 +77,27 @@ module.exports = (api, options, rootOptions) => {
 
   api.render('./template');
 
-  api.afterInvoke(() => {
-    const { EOL } = require('os')
-    const fs = require('fs')
-    const contentMain = fs.readFileSync(api.resolve(api.entryFile), { encoding: 'utf-8' })
-    const lines = contentMain.split(/\r?\n/g)
-
-    const renderIndex = lines.findIndex(line => line.match(/render/))
-    lines[renderIndex] += `${EOL}  router,`
-
-    fs.writeFileSync(api.resolve(api.entryFile), lines.join(EOL), { encoding: 'utf-8' })
-  })
-
   api.onCreateComplete(() => {
     process.env.VUE_CLI_SKIP_WRITE = true
   })
 }
 
-// module.exports.hooks = (api) => {
-//   api.afterInvoke(() => {
-//     const { EOL } = require('os')
-//     const fs = require('fs')
-//     const contentMain = fs.readFileSync(api.resolve(api.entryFile), { encoding: 'utf-8' })
-//     const lines = contentMain.split(/\r?\n/g)
+module.exports.hooks = api => {
+  api.afterInvoke(() => {
+    // Problem with App.vue not being updated correctly.
+    // Workaround at https://github.com/vuejs/vue-cli/issues/5106#issuecomment-579509196 :
+    // vue-router must be added to the "plugins" section of the preset.json file.
+    //
+    //let mainTemplateVuePath = api.resolve('./src/App.tmpl.vue');
+    //let mainVuePath = mainTemplateVuePath.replace(/App\.tmpl\.vue$/, 'App.vue');
+    //fs.unlinkSync(mainVuePath);
+    //fs.renameSync(mainTemplateVuePath, mainVuePath);
 
-//     const renderIndex = lines.findIndex(line => line.match(/render/))
-//     lines[renderIndex] += `${EOL}  router,`
-
-//     fs.writeFileSync(api.resolve(api.entryFile), lines.join(EOL), { encoding: 'utf-8' })
-//   })
-// }
+    if (directoryExits(api.resolve('./src/store'))) {
+      lignator.remove(api.resolve('./src/store'));
+    }
+    if (directoryExits(api.resolve('./src/router'))) {
+      lignator.remove(api.resolve('./src/router'));
+    }
+  });
+};
