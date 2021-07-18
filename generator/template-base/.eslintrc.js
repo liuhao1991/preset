b6a0a3220706353408b1229f1c92b86577e2018e
liuhao <823232600@qq.comm>
2021-07-18 23:19:56 +0800

    update

--- generator/template-base/.eslintrc.js
+++ generator/template-base/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     <%_ if (options.version === 'v3') { _%>
-    "vue/no-multiple-template-root": 0,
+    "vue/no-multiple-template-root": "off",
     <%_ } _%>
   },
 };
