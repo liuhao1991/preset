66318d75079e2cc0c353b2ff9c0c5207491a18b4
liuhao <823232600@qq.comm>
2021-07-18 12:24:45 +0800

    update

--- generator.js
+++ generator.js
@@ -14,90 +14,127 @@ try {
 }
 
 module.exports = (api, options, rootOptions) => {
-  api.render(files => {
-    Object.keys(files)
-      .filter(path => path.startsWith('src/') || path.startsWith('public/'))
-      .forEach(path => delete files[path])
-  });
-  api.extendPackage({
-    'dependencies': {
-      'axios': '^0.19.0',
-      'lodash': '^4.17.15',
-      'normalize.css': '^8.0.1',
-    },
-    'devDependencies': {
-      '@babel/plugin-proposal-optional-chaining': '^7.9.0',
-      // '@commitlint/cli': '^8.3.5',
-      // '@commitlint/config-conventional': '^8.3.4',
-      // 'commitizen': '^4.0.4',
-      'compression-webpack-plugin': '^3.1.0',
-      'cross-env': '^7.0.2',
-      // 'cz-conventional-changelog': '^3.1.0',
-      'vue-cli-plugin-webpack-bundle-analyzer': '~2.0.0',
-    },
-    'scripts': {
-      'build:dev': 'vue-cli-service build --mode development',
-      'build:prod': 'vue-cli-service build --mode production',
-      'test:unit': 'cross-env NODE_ENV=test vue-cli-service test:unit',
-      'test:e2e': 'cross-env NODE_ENV=test vue-cli-service test:e2e',
-      'lint': 'vue-cli-service lint src/**/*.{js,vue} tests/**/*.js --fix'
-    },
-    // 'config': {
-    //   'commitizen': {
-    //     'path': 'node_modules/cz-conventional-changelog'
-    //   }
-    // },
-    // 'gitHooks': {
-    //   'pre-commit': 'lint-staged',
-    //   'commit-msg': 'commitlint -e $GIT_PARAMS'
-    // },
-    // 'lint-staged': {
-    //   'src/**/*.{js,jsx,vue}': [
-    //     'vue-cli-service lint --fix',
-    //     'git add'
-    //   ],
-    //   'tests/**/*.js': [
-    //     'vue-cli-service lint --fix',
-    //     'git add'
-    //   ]
+  // const v2 = options.vueVersion === 'v2';
+    // const v2 = options.version === 'v2';
+    // options.ui = options.ui || 'none';
+
+    // 添加依赖
+    const dependencies = {
+        qs: '^6.7.0',
+        axios: '^0.21.0',
+        'tp-common.css': '^1.0.2',
+    };
+    // // UI 框架
+    // if (options.ui === 'element') {
+    //     if (v2) {
+    //         dependencies['element-ui'] = '^2.15.0';
+    //     } else {
+    //         dependencies['element-plus'] = '^1.0.1-beta.27';
+    //     }
     // }
-  });
+    api.extendPackage({
+        dependencies,
+        devDependencies: {
+            'eslint-config-tpconfig': '^0.x',
+            'eslint-plugin-vue': '^7.0.0-0',
+            'style-resources-loader': '^1.3.2',
+            stylelint: '^13.9.0',
+            '@trscd/stylelint-config-tpconfig': '^0.x',
+            'stylelint-webpack-plugin': '^2.1.1',
+            'webpack-bundle-analyzer': '^4.3.0',
+            'babel-plugin-transform-remove-console': '^6.9.4',
+            'babel-plugin-transform-remove-debugger': '^6.9.4',
+        },
+    });
 
-  // api.extendPackage({
-  //   dependencies: {
-  //     vue: '^3.0.5',
-  //     'vue-router': '^4.0.3'
-  //   },
-  //   devDependencies: {
-  //     '@vue/compiler-sfc': '^3.0.0',
-  //   }
-  // });
+    api.extendPackage({
+        dependencies: {
+            vue: '^3.0.0',
+            'vue-router': '^4.0.0-0',
+            vuex: '^4.0.0-0',
+        },
+        devDependencies: {
+            'vue-template-compiler': null,
+            '@vue/compiler-sfc': '^3.0.0',
+        },
+    });
 
+    // 数据可视化大屏依赖
+    // if (options.type === 'data-v') {
+    //     api.extendPackage({
+    //         dependencies: {
+    //             echarts: '^5.0.0',
+    //             'animate.css': '^4.1.1',
+    //             lodash: '^4.17.21',
+    //             'resize-observer-polyfill': '^1.5.1',
+    //         },
+    //     });
+    // }
 
+    // git hook and commitlint
+    // api.extendPackage({
+    //     scripts: {
+    //         stylelint: 'stylelint \'src/**/*.{vue,html,css,less,scss,sass}\' --fix',
+    //     },
+    //     husky: {
+    //         hooks: {
+    //             'pre-commit': 'lint-staged',
+    //             'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+    //         },
+    //     },
+    //     'lint-staged': {
+    //         '*.{js,vue}': 'npm run lint',
+    //         '*.{vue,html,css,less,scss,sass}': 'npm run stylelint',
+    //     },
+    //     config: {
+    //         commitizen: {
+    //             path: './node_modules/cz-conventional-changelog',
+    //         },
+    //     },
+    //     devDependencies: {
+    //         '@commitlint/cli': '^11.0.0',
+    //         '@commitlint/config-conventional': '^11.0.0',
+    //         'cz-conventional-changelog': '^3.3.0',
+    //         husky: '^4.3.8',
+    //         'lint-staged': '^10.5.3',
+    //     },
+    // });
 
-  api.render('./template');
+    // UI 框架
+    // if (options.ui === 'element') {
+    //     // element 替换主题色需要使用 sass
+    //     api.extendPackage({
+    //         devDependencies: {
+    //             'sass-loader': '^7.1.0',
+    //             'node-sass': '^4.14.1',
+    //         },
+    //     });
+    // }
 
-  api.onCreateComplete(() => {
-    process.env.VUE_CLI_SKIP_WRITE = true
-  })
-}
+    // 创建模板
+    api.render('./template');
 
-module.exports.hooks = api => {
-  api.afterInvoke(() => {
-    // Problem with App.vue not being updated correctly.
-    // Workaround at https://github.com/vuejs/vue-cli/issues/5106#issuecomment-579509196 :
-    // vue-router must be added to the "plugins" section of the preset.json file.
-    //
-    //let mainTemplateVuePath = api.resolve('./src/App.tmpl.vue');
-    //let mainVuePath = mainTemplateVuePath.replace(/App\.tmpl\.vue$/, 'App.vue');
-    //fs.unlinkSync(mainVuePath);
-    //fs.renameSync(mainTemplateVuePath, mainVuePath);
+    // 删除不必要的文件
+    const deletePath = [
+        'src/assets/logo.png',
+        'src/components/HelloWorld.vue',
+    ];
 
-    if (directoryExits(api.resolve('./src/store'))) {
-      lignator.remove(api.resolve('./src/store'));
-    }
-    if (directoryExits(api.resolve('./src/router'))) {
-      lignator.remove(api.resolve('./src/router'));
-    }
-  });
+    api.render(files => {
+        Object.keys(files).forEach(path => {
+            if (deletePath.find(p => path.indexOf(p) === 0)) {
+                delete files[path];
+            }
+        });
+    });
+
+    // 安装的 node-sass 包内缺少 vendor 文件夹
+    // 需要执行 npm rebuild node-sass 生成
+    // if (options.ui === 'element') {
+    //     api.onCreateComplete(() => {
+    //         const exec = require('child_process').execSync;
+    //         const path = api.resolve();
+    //         exec('npm rebuild node-sass', { stdio: 'inherit', cwd: path });
+    //     });
+    // }
 };
