d0c7dd2ad96006dc316a1f37de39896bd8c7a5dc
LH_Inks <823232600@qq.com>
2021-12-31 10:12:50 +0800

    Update main.js

--- generator/template-v3/src/main.js
+++ generator/template-v3/src/main.js
@@ -8,6 +8,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import router from './router'
 <%_ if (options.platform === 'pc') { _%>
 import setupVendor from './vendor/element-v3'
 <%_ } else { _%>
